3ec8537f20929576fccf0c8b0fab4ad4a0d6f005
taekwon <universe134679@gmail.com>
2025-01-02 16:01:10 +0900

    :sparkles: implement Projects/Contact section with draft

--- src/contents/index.jsx
+++ src/contents/index.jsx
@@ -1,3 +1,6 @@
+import pinpoint from '../assets/pin_project.png';
+import snap from '../assets/snap_project.png';
+
 export const HERO_CONTENT = `I am an open-minded Full-Stack Developer. On the front end, I work with React and Next.js, while on the back end, I specialize in Express.js, Node.js, and MongoDB. What excites me most as a developer is not just writing code, but solving problems and growing through the process. I am looking forward to collaborating with global teams in diverse environments. My ultimate goal is to become a developer who grows with the team by leveraging my skills and passion.`
 
 export const ABOUT_TEXT = `Hello! I am Taekwon, a Full-Stack Developer specializing in modern web technologies. My skills are:
@@ -15,3 +18,23 @@ I aim to solve problems and create solutions that make a difference, whether wor
 During the "PinPoint Meet You" project, I took on the role of team leader while being new to web development myself. Guiding a team of non-developers, I focused on fostering collaboration, learning new technologies, and ensuring that everyone contributed to the project’s success.
 
 Although the journey was challenging, it taught me the value of adaptability, perseverance, and effective communication. This experience shaped my ability to lead and collaborate, reinforcing my belief in the power of shared growth. I am excited to bring this mindset to global teams, where diverse perspectives and collective effort drive innovation.`
+
+export const PROJECTS = [
+  {
+    title: "PinPoint Meet You",
+    image: pinpoint,
+    description: `PinPoint`,
+    technologies: ["React", "Express.js", "Node.js", "MongoDB"],
+  },
+  {
+    title: "SnapJournal",
+    image: snap,
+    description: `Portfolio`,
+    technologies: ["Next.js", "Tailwind CSS", "Express.js", "Node.js", "MongoDB"],
+  },
+]
+
+export const CONTACT = {
+  email: "taekwon.kim.dev@gmail.com",
+  address: "Andong, South Korea",
+}
