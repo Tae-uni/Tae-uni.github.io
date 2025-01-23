643a645f332dee2cd946d871d493c2c67681b0ac
taekwon <universe134679@gmail.com>
2025-01-23 14:58:17 +0900

    :green_heart: update deploy and vite config file

--- vite.config.js
+++ vite.config.js
@@ -9,4 +9,15 @@ export default defineConfig({
     host: '0.0.0.0',
     port: 5173,
   },
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+    rollupOptions: {
+      output: {
+        entryFileNames: 'assets/[name].[hash].js',
+        chunkFileNames: 'assets/[name].[hash].js',
+        assetFileNames: 'assets/[name].[hash].[ext]',
+      },
+    },
+  },
 });
